9316a3f9e6ec27a95ddb4ede562cfa0d85b8d505
fikriks <fikriks130@gmail.com>
2023-05-31 11:15:26 +0700

    add: configuring base layouts :rocket:

---
+++ app/Views/layouts/auth.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - Supply Chain Management EOQ Method</title>
+    <link rel="stylesheet" href="assets/css/main/app.css">
+    <link rel="stylesheet" href="assets/css/pages/auth.css">
+    <link rel="shortcut icon" href="assets/images/logo/favicon.svg" type="image/x-icon">
+    <link rel="shortcut icon" href="assets/images/logo/favicon.png" type="image/png">
+</head>
+
+<body>
+    <div id="auth">
+
+        <?= $this->renderSection('content') ?>
+
+    </div>
+</body>
+
+</html>
