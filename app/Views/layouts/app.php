b0bc1bb5701180f8f9840fb529fb5a8fedf555f3
fikriks <fikriks130@gmail.com>
2023-06-07 02:22:42 +0700

    update: title :rocket:

--- app/Views/layouts/app.php
+++ app/Views/layouts/app.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard - Mazer Admin Dashboard</title>
+    <title><?= $this->renderSection('title') ?> - Apotek Erdza Farma</title>
 
     <link rel="stylesheet" href="<?= base_url('assets/css/main/app.css') ?>">
     <link rel="stylesheet" href="<?= base_url('assets/css/main/app-dark.css') ?>">
