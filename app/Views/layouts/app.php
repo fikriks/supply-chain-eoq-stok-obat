7ffe83c78ee523b8efb4e78a7369e9fe47afb197
fikriks <fikriks130@gmail.com>
2023-06-28 00:04:02 +0700

    fix: sidebar color :rocket:

--- app/Views/layouts/app.php
+++ app/Views/layouts/app.php
@@ -20,7 +20,7 @@
 <body>
     <div id="app">
         <div id="sidebar" class="active">
-            <div class="sidebar-wrapper active bg-light">
+            <div class="sidebar-wrapper active">
                 <div class="sidebar-header position-relative">
                     <div class="d-flex justify-content-between align-items-center">
                         <div class="logo">
